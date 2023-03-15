08e787301ead9c59bb4153c2d2e55b55f3f4e883
Rizky Putri <74160733+rizkyputri13@users.noreply.github.com>
2023-03-15 10:49:03 +0700

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,87 @@
+import React from "react"
+import "./index.css"
+
+export default function LoginOrSignUp() {
+  return (
+    <div className="login-or-sign-up clip-contents">
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/2vw3wjfjxze-1%3A34?alt=media&token=ed15cbba-f92c-4d07-a628-e61485ff7250"
+        alt="Not Found"
+        className="email-_black-_-24dp-1"
+      />
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/2vw3wjfjxze-1%3A36?alt=media&token=2bb59cdf-bba3-416b-8739-b127ddb6a708"
+        alt="Not Found"
+        className="group-2968"
+      />
+      <br></br>
+      <p className="enjoy-the-convenience-o">
+        Enjoy the Convenience of <br></br>Beam Space Storage
+      </p>
+      <div className="group-605">
+
+        <div className="group-684">
+          <p className="log-in-to-beam-space">Log in to Beam Space</p>
+          <br></br>
+          <br></br>
+          <div className="group-158">
+            <div className="group-279">
+              {/* <img
+                src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/2vw3wjfjxze-1%3A717?alt=media&token=307b8520-9751-49ef-9c25-d0cc6fc17bee"
+                alt="Not Found"
+                className="group-278"
+              /> */}
+              <br></br>
+              <div className="group-157">
+                <div className="group-156">
+                  <p className="log-in-with-google">Log in with Google</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br></br>
+          <div className="group-281">
+            <div className="group-280">
+              {/* <img
+                src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/2vw3wjfjxze-1%3A726?alt=media&token=2aab8471-c625-41de-8a74-433d15f950ad"
+                alt="Not Found"
+                className="group-48"
+              /> */}
+              <p className="log-in-with-facebook">Log in with Facebook</p>
+            </div>
+          </div>
+          <br></br>
+          <div className="frame-251">
+            <p className="or-login-with-your-email">or login with your email</p>
+          </div>
+          <br></br>
+          <p className="email-address">Email Address*</p>
+          <br></br>
+          <div className="group-845">
+            <p className="egname-email-com">E.g, name@email.com</p>
+          </div>
+          <br></br>
+          <p className="password">Password*</p>
+          <br></br>
+          <div className="group-1110">
+            <p className="enter-your-password">Enter your password</p>
+          </div>
+          <br></br>
+          <div className="frame-14">
+            <p className="login">Login</p>
+            <p className="forgot-password">Forgot Password?</p>
+        <p className="don-thave-an-account-cre">
+          Don’t have an account? Create an account
+        </p>
+          </div>
+        </div>
+        
+      </div>
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/2vw3wjfjxze-1%3A743?alt=media&token=518e0428-ee88-4e6f-960a-e65fb0257105"
+        alt="Not Found"
+        className="frame-175"
+      />
+    </div>
+  )
+}
